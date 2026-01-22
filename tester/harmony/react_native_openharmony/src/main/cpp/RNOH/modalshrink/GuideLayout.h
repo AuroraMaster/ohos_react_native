a47ece96b431144287445c17bfb368f5bbbf0f12
haozhihang <haozhihang@huawei.com>
2026-01-22 21:56:23 +0800

    update: 更新文件 GuideLayout.h
    Signed-off-by: haozhihang <haozhihang@huawei.com>

--- tester/harmony/react_native_openharmony/src/main/cpp/RNOH/modalshrink/GuideLayout.h
+++ tester/harmony/react_native_openharmony/src/main/cpp/RNOH/modalshrink/GuideLayout.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2024 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2026 Huawei Technologies Co., Ltd.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
